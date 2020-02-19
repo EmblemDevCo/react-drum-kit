2024aee12dfd722229102f4e9fcaba681f964f36
Ivan <ivan.herndon@gmail.com>
2020-02-18 22:17:35 -0500

    Set up event listeners for keydown and keyup events. Pass activeKey and keyboard props to DrumPad component. Update DrumPadProps interface to account for new props. Update styles to show key on drum pad and to. Add active class to drum pad to respond to key events.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import DrumPad from './components/DrumPad';
 import './App.css';
 
 function App() {
+  const [activeKey, setActiveKey] = useState();
+  const setActiveKeyHandler = (e: KeyboardEvent) => {
+    setActiveKey(e.key);
+  };
+  const removeActiveKeyHandler = () => setActiveKey('');
+  useEffect(() => {
+    window.addEventListener('keydown', setActiveKeyHandler);
+    window.addEventListener('keyup', removeActiveKeyHandler);
+
+    return () => {
+      window.removeEventListener('keydown', setActiveKeyHandler);
+      window.removeEventListener('keyup', removeActiveKeyHandler);
+    };
+  });
   return (
     <div className="App">
-      <DrumPad sound="sounds/boom.wav"></DrumPad>
+      <DrumPad sound="sounds/boom.wav" activeKey={activeKey} keyboard="a" />
     </div>
   );
 }
--- src/components/DrumPad.tsx
+++ src/components/DrumPad.tsx
@@ -1,14 +1,28 @@
 import React, { useState, useEffect } from 'react';
+import classNames from 'classnames';
 
 interface DrumPadProps {
   sound: string;
+  activeKey: string;
+  keyboard: string;
 }
 
-const DrumPad = ({ sound }: DrumPadProps) => {
+const DrumPad = ({ sound, keyboard, activeKey }: DrumPadProps) => {
   const [audio, setAudio] = useState();
+  const [isActive, setIsActive] = useState();
   useEffect(() => {
     setAudio(new Audio(sound));
-  }, []);
+    if (audio) {
+      if (activeKey === keyboard) {
+        setIsActive(true);
+        audio.play();
+      } else {
+        audio.pause();
+        setIsActive(false);
+        audio.currentTime = 0;
+      }
+    }
+  }, [activeKey]);
 
   const start = () => audio.play();
   const stop = () => {
@@ -16,7 +30,12 @@ const DrumPad = ({ sound }: DrumPadProps) => {
     audio.currentTime = 0;
   };
   return (
-    <div className="drum-pad" onMouseDown={start} onMouseUp={stop}>
+    <div
+      className={classNames('drum-pad', { active: isActive })}
+      onMouseDown={start}
+      onMouseUp={stop}
+    >
+      <span>{keyboard}</span>
       <p></p>
     </div>
   );
